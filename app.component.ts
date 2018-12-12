2658f991b4e839eac380b696f05ad1934cdfe1dc
sairithvikreddy <sairithvikreddyalla@gmail.com>
2018-12-12 10:08:33 -0500

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -7,4 +7,49 @@ import {MovieService} from './services/movie.service';
     templateUrl: 'app.component.html',
     providers: [MovieService]
 })
-export class AppComponent { }
+export class AppComponent { 
+
+
+previousPage() {
+    if (this.page > 1) {
+      // this.jsonData.forEach(row => {
+      //   this.finalJsonDataArray.push(row);
+      // });
+      // this.jsonData = [];
+      this.page = this.page - 1;
+      this.changePage(this.page);
+    }
+  }
+
+  nextPage() {
+    if (this.page < this.numPages()) {
+      this.page = this.page + 1;
+      this.changePage(this.page);
+    }
+  }
+
+  changePage(page) {
+    if (page < 1) {
+      page = 1;
+    }
+    if (page > this.numPages()) {
+      page = this.numPages();
+    }
+    if (page === 1) {
+      for (let i = ((page - 1) * this.records_per_page); i <= (page * this.records_per_page); i++) {
+        if (JsonData[i].key) {
+          this.jsonData.push(JsonData[i]);
+        }
+      }
+    } else {
+      for (let i = ((page - 1) * this.records_per_page); i < (page * this.records_per_page); i++) {
+        if (JsonData[i].key) {
+          this.jsonData.push(JsonData[i]);
+        }
+      }
+    }
+  }
+
+  numPages() {
+    return Math.ceil(JsonData.length / this.records_per_page);
+  }}
